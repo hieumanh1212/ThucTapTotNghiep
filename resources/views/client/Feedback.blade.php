39b0ce9d49e87165d1ac354e3e0993b1b3b6d808
Mạnh Hiếu <121783259+hieumanh1212@users.noreply.github.com>
2024-01-27 14:36:47 +0700

    fix_showFeedback

--- resources/views/client/Feedback.blade.php
+++ resources/views/client/Feedback.blade.php
@@ -91,7 +91,7 @@
                                  </span>
                                     </div>
                                     <div class="tp-contact-info-content">
-                                        <p data-info="mail"><a href="mailto:contact@shofy.com">Nhom07BTLCNPM@gmail.com</a></p>
+                                        <p data-info="mail"><a href="mailto:contact@shofy.com">shofy@gmail.com</a></p>
                                         <p data-info="phone"><a href="tel:670-413-90-762">+ 0912345678</a></p>
                                     </div>
                                 </div>
